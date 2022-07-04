1f9200b99f119606e6347380e7d3d882823ed096
JHLEE <tot0roprog@gmail.com>
2022-07-04 16:16:20 +0900

    feat: Add fast_new_nodes

--- new_node.py
+++ new_node.py
@@ -8,3 +8,9 @@ def new_nodes(api, uuids=[], name="new_node"):
     for i in range(len(uuids)):
         addrs.append(new_node(api, uuids[i], name + str(i)))
     return addrs
+
+def fast_new_nodes(api, uuids=[], name="new_node"):
+    addrs = api('fast_provision_nodes', uuids=uuids)
+    for addr in addrs:
+        api('compose_node', addr)
+    return addrs
